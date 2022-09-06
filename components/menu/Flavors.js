44f2af2928e7d0a8a4060157b518770068c0625b
latz09 <85132328+latz09@users.noreply.github.com>
2022-09-06 15:56:12 -0500

    spacing fixe for flavors

--- components/menu/Flavors.js
+++ components/menu/Flavors.js
@@ -5,7 +5,7 @@ const Flavors = ({ options }) => {
 			{options.map((option) => (
 				<div key={option.type} className='grid gap-1 '>
 					<div className="text-2xl md:text-3xl justify-self-center">{option.type}</div>
-					<div className="flex flex-wrap justify-evenly md:justify-center md:space-x-7 leading-8">{option.flavors.map(flavor => (
+					<div className="flex flex-wrap space-x-4 justify-evenly md:justify-center md:space-x-7 leading-8">{option.flavors.map(flavor => (
                         <span key={'1'}>{flavor}</span>
                     ))}</div>
 				</div>
