acbffe6e6a75e86d15ad4c95f934032afce16f1c
latz09 <85132328+latz09@users.noreply.github.com>
2022-09-06 15:40:07 -0500

    made footer logo link

--- components/layout/footer/Footer.js
+++ components/layout/footer/Footer.js
@@ -7,9 +7,11 @@ import { AiFillInstagram } from 'react-icons/ai';
 const Footer = () => {
 	return (
 		<div className='max-w-7xl mx-auto grid md:grid-cols-2 place-items-center py-6 tracking-widest'>
-			<div>
-				<Logo />
-			</div>
+			<Link href={'/'}>
+				<a>
+					<Logo />
+				</a>
+			</Link>
 			<div className='grid gap-2 place-items-center'>
 				<div className='my-4  grid gap-2 w-full'>
 					<div className='flex space-x-4 md:space-x-8 text-3xl'>
@@ -27,7 +29,6 @@ const Footer = () => {
 					</div>
 				</div>
 			</div>{' '}
-			
 		</div>
 	);
 };
