0aea54c268b500490f898c015059ec6d54640beb
latz09 <85132328+latz09@users.noreply.github.com>
2022-09-06 15:00:46 -0500

    gallery

---
+++ data/galleryImages.js
@@ -0,0 +1,43 @@
+import image1 from '../public/images/cookies/choc-dipped.jpg'
+import image2 from '../public/images/candy/choc-covered.jpg'
+import image3 from '../public/images/cupcakes/3cupcakes.jpg'
+import image4 from '../public/images/cupcakes/oh-baby.jpg'
+import image5 from '../public/images/cupcakes/cake.jpg'
+import image6 from '../public/images/events/celebrate/monsters.jpg'
+import image7 from '../public/images/events/celebrate/reindeer.jpg'
+import image8 from '../public/images/events/celebrate/skates.jpg'
+
+export const galleryImages = [
+    {
+        id: 1,
+        image: image1,
+    },
+    {
+        id: 2,
+        image: image2
+    },
+    {
+        id: 3,
+        image: image3
+    },
+    {
+        id: 4,
+        image: image4
+    },
+    {
+        id: 5,
+        image: image5
+    },
+    {
+        id: 6,
+        image: image6
+    },
+    {
+        id: 7,
+        image: image7
+    },
+    {
+        id: 8,
+        image: image8
+    },
+]
--- pages/our-gallery.js
+++ pages/our-gallery.js
@@ -1,5 +1,22 @@
+import Image from 'next/image';
+import { galleryImages } from '../data/galleryImages';
+
 const Gallery = () => {
-    return ( <div>Gallery</div> );
-}
- 
+	return (
+		<>
+        <h1 className="text-center py-8 text-5xl tracking-widest text-juanjosTeal ">Inspiration</h1>
+			<div className='max-w-7xl mx-auto grid md:grid-cols-3 place-items-center md:gap-4 '>
+				{galleryImages.map((image) => (
+					<div
+						key={image.id}
+						className='grid items-center  scale-90 md:scale-75 shadow hover:shadow-xl hover:scale-110 transition duration-1000'
+					>
+						<Image src={image.image} alt='' layout='intrinsic' />
+					</div>
+				))}
+			</div>
+		</>
+	);
+};
+
 export default Gallery;
