16fb73e060d0349915c4b6cedcbba67a2c08385a
latz09 <85132328+latz09@users.noreply.github.com>
2022-09-06 16:00:56 -0500

    d

--- components/menu/Flavors.js
+++ components/menu/Flavors.js
@@ -3,9 +3,9 @@ const Flavors = ({ options }) => {
 	return (
 		<div className="grid gap-5">
 			{options.map((option) => (
-				<div key={option.type} className='grid gap-1 '>
+				<div key={option.type} className='grid gap-1'>
 					<div className="text-2xl md:text-3xl justify-self-center">{option.type}</div>
-					<div className="flex flex-wrap space-x-4 justify-evenly md:justify-center md:space-x-7 leading-8">{option.flavors.map(flavor => (
+					<div className="flex flex-wrap  sm:space-x-2 justify-evenly md:justify-center leading-8">{option.flavors.map(flavor => (
                         <span key={'1'}>{flavor}</span>
                     ))}</div>
 				</div>
